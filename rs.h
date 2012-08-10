3ae2824b7787996cbcb879d12cf5cfcfde541c4d
yym524 <529008360@qq.com>
2012-08-10 15:17:01 +0800

    modified the server file

--- rs.h
+++ rs.h
@@ -19,25 +19,28 @@ int sys_err(const char *str){
 
 void server_recv(int sock_fd){
      char buf[BUF_SIZE]="", start[BUF_SIZE] = "";
-     int cnt;
+     char send_buf[BUF_SIZE]="";
+     int cnt,send_num;
      int i = 0;
      while(1){
           cnt = recv(sock_fd, buf, BUF_SIZE, 0);
           if(cnt == -1)
                sys_err("recv error!\n");
-          else if(cnt < BUF_SIZE){
-               if(memcmp(buf,"close",5) == 0){
-                    printf("Recieved exit command!\n//-*-&-*-&- Server -*-*-*- Closed! -&-*-&-*//\n");
-                    exit(1);
-               }
-               else{
-                    printf("%s\n//-*-&-*-&- OVER! -&-*-&-*-//\n",buf);
-                    exit(0);
-               }
-          }
-          else if(cnt == BUF_SIZE){
+          else{
+               printf("//-*-&-*-&- Begin Recieve -&-*-&-*-//\n");
                printf("%s",buf);
+               printf("\n//-*-&-*-&- Recieved Over! -&-*-&-*-//\n");
                memcpy(buf, start, sizeof(char) * BUF_SIZE);
-          }      
+          }
+          printf("//-*-&-*-&- Begin Send -&-*-&-*-//\n");
+          printf("Please input words you want say:\n");
+          //scanf("%50[^\n]",send_buf);
+          fgets(send_buf,255,stdin);
+          send_num = send(sock_fd,send_buf,sizeof(send_buf),0);
+          if(send_num < 0)
+               sys_err("send error!\n");
+          else 
+               printf("send success:%s\n",send_buf);
+          memcpy(send_buf,start,sizeof(char) * BUF_SIZE);
      }
 }
